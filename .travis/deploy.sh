2f6f6f16a641e85ea26560a14a7e61e881b34438
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-03-21 22:32:11 +0530

    fix build settings

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -15,7 +15,6 @@ mkdir public
 
 # copy required files
 cp ./examples/index.html ./public/index.html
-cp ./examples/notie.css ./examples/notie.css
 npm run build:examples && cp ./examples/bundle.js ./public/bundle.js
 
 cd public
