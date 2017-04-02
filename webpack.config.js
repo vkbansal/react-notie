1b8a6368f3414298aba876c706e74b65af2c741c
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-04-02 09:46:50 +0530

    fix babel settings for umd build

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
                             ]
                         ],
                         plugins: [
-                            'transform-class-properties'
+                            'transform-class-properties',
+                            'transform-object-rest-spread'
                         ]
                     },
                 }],
