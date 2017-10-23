0daa966ee2fe40b16e2638df681c0cdf9a24ffb6
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-10-23 10:50:06 +0530

    update tests

--- tests/Notie.test.js
+++ tests/Notie.test.js
@@ -27,40 +27,48 @@ describe('<Notie />', () => {
 
     test('success message', () => {
         component.instance().success('Success!');
+        component.update();
         expect(component.find('.react-notie-level--success').length).toBe(1);
         expect(component.find('.react-notie--active').length).toBe(1);
         expect(component).toMatchSnapshot();
         jest.runTimersToTime(5000);
+        component.update();
         expect(component.find('.react-notie--active').length).toBe(0);
         expect(component).toMatchSnapshot();
     });
 
     test('warn message', () => {
         component.instance().warn('Warn!');
+        component.update();
         expect(component.find('.react-notie-level--warn').length).toBe(1);
         expect(component.find('.react-notie--active').length).toBe(1);
         expect(component).toMatchSnapshot();
         jest.runTimersToTime(5000);
+        component.update();
         expect(component.find('.react-notie--active').length).toBe(0);
         expect(component).toMatchSnapshot();
     });
 
     test('error message', () => {
         component.instance().error('Error!');
+        component.update();
         expect(component.find('.react-notie-level--error').length).toBe(1);
         expect(component.find('.react-notie--active').length).toBe(1);
         expect(component).toMatchSnapshot();
         jest.runTimersToTime(5000);
+        component.update();
         expect(component.find('.react-notie--active').length).toBe(0);
         expect(component).toMatchSnapshot();
     });
 
     test('info message', () => {
         component.instance().info('Info!');
+        component.update();
         expect(component.find('.react-notie-level--info').length).toBe(1);
         expect(component.find('.react-notie--active').length).toBe(1);
         expect(component).toMatchSnapshot();
         jest.runTimersToTime(5000);
+        component.update();
         expect(component.find('.react-notie--active').length).toBe(0);
         expect(component).toMatchSnapshot();
     });
@@ -68,10 +76,12 @@ describe('<Notie />', () => {
     test('consecutive calls', () => {
         const instance = component.instance();
         instance.info('Info!');
+        component.update();
         expect(component.find('.react-notie-level--info').length).toBe(1);
         expect(component.find('.react-notie--active').length).toBe(1);
         expect(component).toMatchSnapshot();
         jest.runTimersToTime(2000);
+        component.update();
         instance.error('Error!');
 
         /*
@@ -81,6 +91,7 @@ describe('<Notie />', () => {
         */
         instance.transitionendCallback();
         expect(instance.transitionendCallback).toBe(null);
+        component.update();
 
         expect(component.find('.react-notie-level--error').length).toBe(1);
         expect(component.find('.react-notie--active').length).toBe(1);
@@ -92,16 +103,19 @@ describe('<Notie />', () => {
         instance.info('Info!');
         instance.info('Info!');
         jest.runTimersToTime(2000);
+        component.update();
         component.unmount();
         expect(instance.transitionendCallback).toBe(null);
     });
 
     test('dismiss click', () => {
         component.instance().info('Info!');
+        component.update();
         expect(component.find('.react-notie-level--info').length).toBe(1);
         expect(component.find('.react-notie--active').length).toBe(1);
         expect(component).toMatchSnapshot();
         component.find('.react-notie-dismiss').simulate('click');
+        component.update();
         expect(component.find('.react-notie--active').length).toBe(0);
         expect(component).toMatchSnapshot();
     });
@@ -109,6 +123,7 @@ describe('<Notie />', () => {
     describe('confirm', () => {
         test('initial render', () => {
             component.instance().confirm('Are you sure?');
+            component.update();
             expect(component.find('.react-notie-level--confirm').length).toBe(1);
             expect(component.find('.react-notie--active').length).toBe(1);
             expect(component).toMatchSnapshot();
@@ -118,9 +133,12 @@ describe('<Notie />', () => {
             const instance = component.instance();
             const promise = instance.confirm('Are you sure?');
 
+            component.update();
             component.find('.react-notie-choice--yes').simulate('click');
             instance.transitionendCallback();
             expect(instance.transitionendCallback).toBe(null);
+            component.update();
+
             promise.then(() => {
                 expect(component).toMatchSnapshot();
                 expect(component.find('.react-notie--active').length).toBe(0);
@@ -132,9 +150,11 @@ describe('<Notie />', () => {
             const instance = component.instance();
             const promise = instance.confirm('Are you sure?');
 
+            component.update();
             component.find('.react-notie-choice--no').simulate('click');
             instance.transitionendCallback();
             expect(instance.transitionendCallback).toBe(null);
+            component.update();
             promise.catch(() => {
                 expect(component).toMatchSnapshot();
                 expect(component.find('.react-notie--active').length).toBe(0);
@@ -145,9 +165,11 @@ describe('<Notie />', () => {
         test('no consecutive calls', () => {
             const instance = component.instance();
             instance.confirm('Are you sure?');
+            component.update();
             expect(component.find('.react-notie-level--confirm').length).toBe(1);
             expect(component).toMatchSnapshot();
             instance.info('Info!');
+            component.update();
             expect(component.find('.react-notie-level--confirm').length).toBe(1);
             expect(component).toMatchSnapshot();
         });
